a5d6be827377991b24cf8c18ff9bffe6a7d6e851
maryvlasenko10@gmail.com <maryvlasenko10@gmail.com>
2023-01-16 19:43:18 +0300

    Лабораторная работа №1

--- Лаба№1/Лаба№1/Form1.cs
+++ Лаба№1/Лаба№1/Form1.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Лаба_1
 {
@@ -15,6 +7,14 @@ namespace Лаба_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Вывод в заголовок экрана
+            Text = string.Format("Координаты: {0}, {1}", e.X, e.Y);
+            //Сумма координат
+            textBox1.Text = (e.X + e.Y).ToString();
         }
     }
 }
